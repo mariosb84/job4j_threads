f58d955e3ce0ca8b71bdcf9c52eec0c7dbf59905
Sergei Baranov <mariosb84@mail.ru>
2022-05-25 16:41:08 +0300

    3. Прерывание нити [#1019 #304680]

--- src/main/java/ru/job4j/concurrent/ConsoleProgress.java
+++ src/main/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -13,7 +13,6 @@ public class ConsoleProgress implements Runnable {
             try {
                 Thread.sleep(500);
             } catch (InterruptedException e) {
-                /*e.printStackTrace();*/
                 Thread.currentThread().interrupt();
                 }
              }
@@ -22,7 +21,7 @@ public class ConsoleProgress implements Runnable {
     public static void main(String[] args) throws InterruptedException {
         Thread progress = new Thread(new ConsoleProgress());
         progress.start();
-        Thread.sleep(1000); /* симулируем выполнение параллельной задачи в течение 1 секунды. */
+        Thread.sleep(1000);
         progress.interrupt();
     }
 }
