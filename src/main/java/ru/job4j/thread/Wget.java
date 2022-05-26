9fb7b82b999c688351e190152bd5ebec0a3188f7
Sergei Baranov <mariosb84@mail.ru>
2022-05-26 16:56:33 +0300

    4. Скачивание файла с ограничением. [#144271]

--- src/main/java/ru/job4j/thread/Wget.java
+++ src/main/java/ru/job4j/thread/Wget.java
@@ -1,5 +1,10 @@
 package ru.job4j.thread;
 
+import java.io.BufferedInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URL;
+
 public class Wget implements Runnable {
     private final String url;
     private final int speed;
@@ -11,10 +16,28 @@ public class Wget implements Runnable {
 
     @Override
     public void run() {
-        /* Скачать файл*/
+        /* Задержка, исходя из скорости  в КБайт/сек*/
+        int delay = (1024 / 1000) / this.speed;
+        try (BufferedInputStream in = new BufferedInputStream(new URL(this.url).openStream());
+             FileOutputStream fileOutputStream = new FileOutputStream("pom_tmp.xml")) {
+            byte[] dataBuffer = new byte[1024];
+            int bytesRead;
+            while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
+                fileOutputStream.write(dataBuffer, 0, bytesRead);
+                if (delay >= 1) {
+                    Thread.sleep(delay);
+                }
+            }
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) throws InterruptedException {
+        if (args.length != 2) {
+            throw new IllegalArgumentException(
+                    "url is null, speed is null.");
+        }
         String url = args[0];
         int speed = Integer.parseInt(args[1]);
         Thread wget = new Thread(new Wget(url, speed));
