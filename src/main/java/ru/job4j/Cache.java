449f6788679c4a6c0dfa1a04606751854a6a1dea
Sergei Baranov <mariosb84@mail.ru>
2022-05-31 14:40:18 +0300

    1. Синхронизация общих ресурсов. [#1096]

---
+++ src/main/java/ru/job4j/Cache.java
@@ -0,0 +1,31 @@
+package ru.job4j;
+
+public final class Cache {
+
+    private static Cache cache;
+
+    public synchronized static Cache instOf() {
+        if (cache == null) {
+            cache = new Cache();
+        }
+        return cache;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread first = new Thread(
+                () -> {
+                    try {
+                        Thread.sleep(1000);
+                        System.out.println(instOf());
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                });
+        Thread second = new Thread(
+                () -> System.out.println(instOf()));
+        first.start();
+        second.start();
+        first.join();
+        second.join();
+    }
+}
