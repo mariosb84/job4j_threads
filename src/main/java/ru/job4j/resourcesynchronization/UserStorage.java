f252526eca2af08fd99eed30980e3b9e3da02867
Sergei Baranov <mariosb84@mail.ru>
2022-06-10 10:37:50 +0300

    3. Класс хранилища пользователей UserStorage [#1104 #310428]

--- src/main/java/ru/job4j/resourcesynchronization/UserStorage.java
+++ src/main/java/ru/job4j/resourcesynchronization/UserStorage.java
@@ -10,33 +10,23 @@ public class UserStorage {
     private final Map<Integer, User> userStorages = new HashMap<>();
 
     public synchronized boolean add(User user) {
-        if (!userStorages.containsKey(user.getId())) {
-            userStorages.putIfAbsent(user.getId(), user);
-            return true;
-        }
-        return false;
+        return userStorages.putIfAbsent(user.getId(), user) != null;
     }
 
     public synchronized boolean update(User user) {
-        if (userStorages.containsKey(user.getId())) {
-            userStorages.replace(user.getId(), user);
-            return true;
-        }
-        return false;
+        return userStorages.replace(user.getId(), user) != null;
     }
 
     public synchronized boolean delete(User user) {
-        if (userStorages.containsKey(user.getId())) {
-            userStorages.remove(user.getId());
-            return true;
-        }
-        return false;
+        return userStorages.remove(user.getId()) != null;
     }
 
     public synchronized boolean transfer(int fromId, int toId, int amount) {
         User userFrom = userStorages.get(fromId);
         User userTo = userStorages.get(toId);
-        if (userFrom.getAmount() >= amount) {
+        if (userFrom != null
+                && userTo != null
+                && userFrom.getAmount() >= amount) {
          userFrom.setAmount(userFrom.getAmount() - amount);
          userTo.setAmount(userTo.getAmount() + amount);
          return true;
