54472ab4389c68846f849211732ca1cc46d41cfe
Sergei Baranov <mariosb84@mail.ru>
2022-06-16 12:38:09 +0300

    4. ThreadSafe динамический список [#1105]

--- src/main/java/ru/job4j/resourcesynchronization/SingleLockList.java
+++ src/main/java/ru/job4j/resourcesynchronization/SingleLockList.java
@@ -1,20 +1,26 @@
 package ru.job4j.resourcesynchronization;
 
+import com.google.gson.Gson;
 import net.jcip.annotations.GuardedBy;
 import net.jcip.annotations.ThreadSafe;
-import  java.lang.Cloneable;
 
+import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
 @ThreadSafe
 public class SingleLockList<T> implements Iterable<T> {
     @GuardedBy("this")
-    private final List<T> list;
+    private final List<T> list = new ArrayList<>();
 
-    public SingleLockList(List<T> list) {
-        this.list = (List) list.clone();
+    public  SingleLockList() {
     }
+
+    /*public SingleLockList(List<T> list) throws CloneNotSupportedException {
+        this.list = (List) list.clone();
+        this.list = (List<T>) super.clone();
+    }*/
 
     public synchronized void add(T value) {
         list.add(value);
@@ -26,16 +32,17 @@ public class SingleLockList<T> implements Iterable<T> {
 
     @Override
     public synchronized Iterator<T> iterator() {
-        return (Iterator<T>) copy(this.list).iterator();
+        return copy(this.list).iterator();
     }
 
-    private Iterable<Object> copy(List<T> list) {
-        return (Iterable<Object>) list.iterator();
-    }
-
-    @Override
-    public Object clone() throws CloneNotSupportedException {
-        return new SingleLockList((List) this);
+    private List<T> copy(List<T> list) {
+        List<T> listFinal = new ArrayList<>();
+        for (T value : list) {
+            Gson gson = new Gson();
+            T deepCopiedT = gson.fromJson(gson.toJson(value), (Type) value.getClass());
+            listFinal.add(deepCopiedT);
+        }
+        return listFinal;
     }
 
 }
