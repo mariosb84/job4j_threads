ee03fb80272be3bfdfc10f6a7a6daae04e6a415e
Sergei Baranov <mariosb84@mail.ru>
2022-07-04 12:54:34 +0300

    1. Реализовать ThreadPool [#1099 #318315]

--- src/main/java/ru/job4j/pools/ThreadPool.java
+++ src/main/java/ru/job4j/pools/ThreadPool.java
@@ -5,12 +5,29 @@ import ru.job4j.waitnotifynotifyall.SimpleBlockingQueue;
 import java.util.LinkedList;
 import java.util.List;
 
-public class ThreadPool implements Runnable {
+public class ThreadPool {
 
+    private static final int LIMIT = Integer.MAX_VALUE;
     private final List<Thread> threads = new LinkedList<>();
-    private final SimpleBlockingQueue<Runnable> tasks = new SimpleBlockingQueue<>(3);
+    private final SimpleBlockingQueue<Runnable> tasks = new SimpleBlockingQueue<>(LIMIT);
 
     public ThreadPool() {
+        int size = Runtime.getRuntime().availableProcessors();
+        for (int i = 0; i < size; i++) {
+            Thread thread = new Thread(
+                    () -> {
+                        while (!Thread.currentThread().isInterrupted()) {
+                            try {
+                                tasks.poll().run();
+                            } catch (InterruptedException e) {
+                                Thread.currentThread().interrupt();
+                            }
+                        }
+                    }
+            );
+            thread.start();
+            threads.add(thread);
+        }
     }
 
     public void work(Runnable job) throws InterruptedException {
@@ -23,23 +40,4 @@ public class ThreadPool implements Runnable {
         }
     }
 
-    @Override
-    public void run() {
-        int size = Runtime.getRuntime().availableProcessors();
-        for (int i = 0; i < size; i++) {
-            Thread thread = new Thread(
-                    () -> {
-                        while (!Thread.currentThread().isInterrupted()) {
-                            try {
-                                tasks.poll();
-                            } catch (InterruptedException e) {
-                                Thread.currentThread().interrupt();
-                            }
-                        }
-                    }
-            );
-            thread.start();
-            threads.add(thread);
-        }
-    }
 }
