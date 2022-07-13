086fd27962e260340039358e5f38518b3c14f61e
Sergei Baranov <mariosb84@mail.ru>
2022-07-13 17:40:38 +0300

    4. CompletableFuture [#361626]

---
+++ src/main/java/ru/job4j/pools/RolColSum.java
@@ -0,0 +1,35 @@
+package ru.job4j.pools;
+
+public class RolColSum {
+
+    public static class Sums {
+        private int rowSum;
+        private int colSum;
+
+        public int getRowSum() {
+            return rowSum;
+        }
+
+        public void setRowSum(int rowSum) {
+            this.rowSum = rowSum;
+        }
+
+        public int getColSum() {
+            return colSum;
+        }
+
+        public void setColSum(int colSum) {
+            this.colSum = colSum;
+        }
+    }
+
+
+    public static Sums[] sum(int[][] matrix) {
+
+    }
+
+    public static Sums[] asyncSum(int[][] matrix) {
+
+    }
+
+}
