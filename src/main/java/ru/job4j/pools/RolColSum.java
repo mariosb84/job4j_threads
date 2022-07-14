9582ed92f4c8e0122980e65eb14a43ea9b8d74c3
Sergei Baranov <mariosb84@mail.ru>
2022-07-14 15:22:55 +0300

    4. CompletableFuture [#361626]

--- src/main/java/ru/job4j/pools/RolColSum.java
+++ src/main/java/ru/job4j/pools/RolColSum.java
@@ -1,5 +1,10 @@
 package ru.job4j.pools;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
 public class RolColSum {
 
     public static class Sums {
@@ -25,11 +30,37 @@ public class RolColSum {
 
 
     public static Sums[] sum(int[][] matrix) {
-
+        Sums[] sums = new Sums[matrix.length];
+        for (int i = 0; i < matrix.length; i++) {
+            sums[i] = getSums(matrix, i);
+        }
+        return sums;
     }
 
-    public static Sums[] asyncSum(int[][] matrix) {
+    private static Sums getSums(int[][] matrix, int i) {
+        Sums sum = new Sums();
+        for (int j = 0; j < matrix.length; j++) {
+            sum.rowSum += matrix[i][j];
+            sum.colSum += matrix[j][i];
+        }
+        return sum;
+    }
 
+    public static Sums[] asyncSum(int[][] matrix) throws ExecutionException, InterruptedException {
+        Sums[] sums = new Sums[matrix.length];
+        int n = matrix.length;
+        Map<Integer, CompletableFuture<Sums>> futures = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            futures.put(i, getTask(matrix, i));
+        }
+        for (Integer key : futures.keySet()) {
+            sums[key] = futures.get(key).get();
+        }
+        return sums;
+    }
+
+    private static CompletableFuture<Sums> getTask(int[][] matrix, int i) {
+        return CompletableFuture.supplyAsync(() -> getSums(matrix, i));
     }
 
 }
--- src/test/java/ru/job4j/RolColSumTest.java
+++ src/test/java/ru/job4j/RolColSumTest.java
@@ -1,4 +1,43 @@
 package ru.job4j;
 
+import org.junit.Test;
+import ru.job4j.pools.RolColSum;
+
+import java.util.concurrent.ExecutionException;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
 public class RolColSumTest {
+    @Test
+    public void whenSubsequentSum() {
+        int[][] matrix = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+        RolColSum.Sums[] sums = RolColSum.sum(matrix);
+        assertThat(sums[0].getRowSum(), is(6));
+        assertThat(sums[0].getColSum(), is(12));
+        assertThat(sums[1].getRowSum(), is(15));
+        assertThat(sums[1].getColSum(), is(15));
+        assertThat(sums[2].getRowSum(), is(24));
+        assertThat(sums[2].getColSum(), is(18));
+    }
+
+    @Test
+    public void whenAsyncSum() throws InterruptedException, ExecutionException {
+        int[][] matrix = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+        RolColSum.Sums[] sums = RolColSum.asyncSum(matrix);
+        assertThat(sums[0].getRowSum(), is(6));
+        assertThat(sums[0].getColSum(), is(12));
+        assertThat(sums[1].getRowSum(), is(15));
+        assertThat(sums[1].getColSum(), is(15));
+        assertThat(sums[2].getRowSum(), is(24));
+        assertThat(sums[2].getColSum(), is(18));
+    }
 }
